775cffececc0f73b8258ab4fcf07ae7be0c5e815
Malaika-Naveed <f223629@cfd.nu.edu.pk>
2024-10-05 08:32:30 +0500

    Added Library class function add book and list book

---
+++ src/scd/Library.java
@@ -0,0 +1,17 @@
+package scd;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Library {
+	List<String> books = new ArrayList<>();
+
+	public void addBokk(String title) {
+		books.add(title);
+		System.out.println(title + "Book Added to Library.");
+	}
+
+	public void listBooks() {
+		System.out.println("Books in Library: " + books);
+	}
+}
